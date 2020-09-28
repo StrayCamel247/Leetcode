b2c2b277f0f4a24f13510a1c10cc3ced586163da
Freen <aboyinsky@outlook.com>
2020-09-28 18:05:30 +0800

    旋转数组

--- questions/189.旋转数组.py
+++ questions/189.旋转数组.py
@@ -41,12 +41,25 @@
 # 
 # 
 #
-
+from typing import List
 # @lc code=start
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # for i in range(k):
+        #     nums.insert(0, nums[-1])
+        #     nums.pop()
+        n=len(nums)
+        k=k%n
+        def swap(l,r):
+            while(l<r):
+                nums[l],nums[r]=nums[r],nums[l]
+                l+=1
+                r-=1
+        swap(0,n-k-1)
+        swap(n-k,n-1)
+        swap(0,n-1)
 # @lc code=end
 
