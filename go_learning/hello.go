0d569b3d4fc403eefaf7c95bdfaac76a50d167cd
Freen <aboyinsky@outlook.com>
2021-03-05 17:49:52 +0800

    go string_handler

--- go_learning/hello.go
+++ go_learning/hello.go
@@ -1,11 +1,35 @@
 package main
 
 import (
-	"fmt"
-
-	"rsc.io/quote"
+	"reflect"
 )
 
+type decorator func()
+
+func (this decorator) invoke() {
+	println("before")
+	this()
+	println("after")
+}
+
+func myFunc(s string) {
+	println(s)
+}
+
+func call(func_name interface{}, params ...interface{}) func() {
+	f := func() {
+		var fc = reflect.ValueOf(func_name)
+		in := make([]reflect.Value, len(params))
+		for k, param := range params {
+			in[k] = reflect.ValueOf(param)
+		}
+		fc.Call(in)
+	}
+	return f
+}
+
 func main() {
-	fmt.Println(quote.Go())
+	f := call(myFunc, "hh")
+	x := decorator(f)
+	x.invoke()
 }
