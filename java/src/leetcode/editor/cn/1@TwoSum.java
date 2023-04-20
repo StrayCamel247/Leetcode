87fbc2deb3bd554a1b792e8c44541443bf3b6b18
StrayCamel247 <aboyinsky@outlook.com>
2023-04-21 00:22:35 +0800

    java two sum

---
+++ java/src/leetcode/editor/cn/1@TwoSum.java
@@ -0,0 +1,80 @@
+package leetcode.editor.cn;
+/*
+ * @Author https://github.com/StrayCamel247
+ * @Date 2023-04-21 00:21:25
+ * https://leetcode.cn/problems/two-sum/
+ * */
+
+//给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那 两个 整数，并返回它们的数组下标。 
+//
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。 
+//
+// 你可以按任意顺序返回答案。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [2,7,11,15], target = 9
+//输出：[0,1]
+//解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [3,2,4], target = 6
+//输出：[1,2]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [3,3], target = 6
+//输出：[0,1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 2 <= nums.length <= 10⁴ 
+// -10⁹ <= nums[i] <= 10⁹ 
+// -10⁹ <= target <= 10⁹ 
+// 只会存在一个有效答案 
+// 
+//
+// 
+//
+// 进阶：你可以想出一个时间复杂度小于 O(n²) 的算法吗？ 
+//
+// Related Topics 数组 哈希表 👍 16856 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    res[0] = i;
+                    res[1] = j;
+                    return res;
+                }
+            }
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+class TwoSum {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
+}
---
+++ test.bat
@@ -0,0 +1,10 @@
+@echo off
+set "folder=..\Leetcode\java"
+set "output=SolvedViaJava.md"
+
+if exist "%output%" del "%output%"
+
+for /r "%folder%" %%f in (*) do (
+  echo %%~nxf|findstr /c:".java"> nul 2>nul &&  echo %%~nxf|findstr /c:"@"> nul 2>nul && echo solve via java && echo %%~nxf && echo %%~nxf >> "%output%"
+)
+
